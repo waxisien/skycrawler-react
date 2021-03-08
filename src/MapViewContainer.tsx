b3ab3e55ad947cc18c964585b74474dfd19a53b9
waxisien <flo.nitzsche@gmail.com>
2021-03-07 20:39:22 -0500

    Reactivate filters

--- src/MapViewContainer.tsx
+++ src/MapViewContainer.tsx
@@ -7,12 +7,17 @@ import MapView from "src/MapView";
 const MapViewContainer = (): JSX.Element => {
   const defaultCenter: LatLngLiteral = { lat: 40, lng: 30 };
   const defaultZoom = 2;
+  const minZoom = 2;
+  const maxZoom = 7;
 
   return (
     <MapContainer
       center={defaultCenter}
       zoom={defaultZoom}
+      maxZoom={maxZoom}
+      minZoom={minZoom}
       scrollWheelZoom={true}
+      zoomControl={false}
     >
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
